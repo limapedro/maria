99e23936e76d60d0fe50be6f6ae7cc4ee1400eea
limapedro <Pedrolimasilva17@gmail.com>
2020-12-26 22:37:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, fram
 stream.start_stream()
 
 while True:
-    data = stream.read(2000)
+    data = stream.read(4000)
     if len(data) == 0:
         break
     if rec.AcceptWaveform(data):
